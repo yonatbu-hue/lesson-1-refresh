2cad8fb7eb13db59280add354bb88a50e53dfb98
yonatbu-hue <yonatbu@ac.sce.ac.il>
2026-05-07 11:48:46 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,3 +1,8 @@
 def factors(x):
-  # your code here
+  # your code here# מעבר על כל המספרים מ-1 ועד x (כולל)
+    for i in range(1, x + 1):
+        # בדיקה האם x מתחלק ב-i ללא שארית
+        if x % i == 0:
+            result.append(i
+    return result
   return a_list
